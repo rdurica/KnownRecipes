f9b176882206fb2c4d393c28e7e3ece5c3623bfe
Robert Durica <r.durica@gmail.com>
2022-06-28 23:21:16 +0200

    Initial Commit

---
+++ KnownRecipes/KnownRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnownRecipesApp.swift
+//  KnownRecipes
+//
+//  Created by Robert Ďurica on 28.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct KnownRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
